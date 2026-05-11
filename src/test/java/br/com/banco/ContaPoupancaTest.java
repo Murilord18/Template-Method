a156603ce09bd764b6fcdf4be0bc16c82d7788c3
Murilo Schittino <146205147+Murilord18@users.noreply.github.com>
2026-05-11 12:40:53 -0300

    Add Test CalcularTarifaMensalCorretamente

--- src/test/java/br/com/banco/ContaPoupancaTest.java
+++ src/test/java/br/com/banco/ContaPoupancaTest.java
@@ -1,4 +1,15 @@
 package br.com.banco;
 
+import br.com.banco.templatemethod.ContaCorrente;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ContaPoupancaTest {
+
+    @Test
+    void deveCalcularTarifaMensalCorretamente() {
+        ContaCorrente conta = new ContaCorrente("001-1", "João Silva", 1000.00);
+        assertEquals(20.00, conta.calcularTarifaMensal(), 0.001);
+    }
 }
