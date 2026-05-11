5fc951c7ede92b88f559a03ad2f3e308f63b038b
Murilo Schittino <146205147+Murilord18@users.noreply.github.com>
2026-05-11 12:37:00 -0300

    Update metodo PercentualTarifa e TipoConta

--- src/main/java/br/com/banco/templatemethod/ContaCorrente.java
+++ src/main/java/br/com/banco/templatemethod/ContaCorrente.java
@@ -1,4 +1,18 @@
 package br.com.banco.templatemethod;
 
-public class ContaCorrente {
+public class ContaCorrente extends ContaBancaria {
+
+    public ContaCorrente(String numeroConta, String titular, double saldo) {
+        super(numeroConta,titular,saldo);
+    }
+
+    @Override
+    public double getPercentualTarifa() {
+        return 0.02;
+    }
+
+    @Override
+    public String getTipoConta() {
+        return "ContaCorrente";
+    }
 }
