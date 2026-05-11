9e07736a7af1c3223734a82527ae86aaa3c81d66
Murilo Schittino <146205147+Murilord18@users.noreply.github.com>
2026-05-11 12:49:47 -0300

    Add ContaCorrenteTest

--- src/test/java/br/com/banco/ContaCorrenteTest.java
+++ src/test/java/br/com/banco/ContaCorrenteTest.java
@@ -1,4 +1,42 @@
-package br.com.banco;
+package br.com.banco.templatemethod;
 
-public class ContaCorrenteTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContaCorrenteTest {
+
+    @Test
+    void deveCalcularTarifaMensalCorretamente() {
+        ContaCorrente conta = new ContaCorrente("001-1", "João Silva", 1000.00);
+        assertEquals(20.00, conta.calcularTarifaMensal(), 0.001);
+    }
+
+    @Test
+    void deveRetornarSaldoSuficienteQuandoHaSaldo() {
+        ContaCorrente conta = new ContaCorrente("001-2", "Maria Souza", 500.00);
+        assertEquals("Saldo suficiente", conta.verificarSaldoAposCobranca());
+    }
+
+    @Test
+    void deveRetornarSaldoInsuficienteQuandoSaldoZerado() {
+        ContaCorrente conta = new ContaCorrente("001-3", "Pedro Costa", 0.00);
+        assertEquals("Saldo suficiente", conta.verificarSaldoAposCobranca());
+    }
+
+    @Test
+    void deveRetornarExtratoCorreto() {
+        ContaCorrente conta = new ContaCorrente("002-1", "Ana Lima", 1000.00);
+        String extrato = conta.getExtrato();
+        assertEquals(
+                "ContaCorrente{numeroConta='002-1', titular='Ana Lima', saldo=1000.00, tarifaMensal=20.00, status='Saldo suficiente'}",
+                extrato
+        );
+    }
+
+    @Test
+    void deveRetornarTipoContaCorrente() {
+        ContaCorrente conta = new ContaCorrente("003-1", "Carlos Mendes", 200.00);
+        assertEquals("ContaCorrente", conta.getTipoConta());
+    }
 }
