864eb943874b4a7e9a876ec2cd05d179df2a2c89
Murilo Schittino <146205147+Murilord18@users.noreply.github.com>
2026-05-11 12:38:46 -0300

    Add PercentualTarifa and TipoConta

--- src/main/java/br/com/banco/templatemethod/ContaPoupanca.java
+++ src/main/java/br/com/banco/templatemethod/ContaPoupanca.java
@@ -1,4 +1,19 @@
 package br.com.banco.templatemethod;
 
-public class ContaPoupanca {
+public class ContaPoupanca extends ContaBancaria {
+
+    public ContaPoupanca(String numeroConta, String titular, double saldo) {
+        super(numeroConta, titular, saldo);
+    }
+
+
+    @Override
+    public double getPercentualTarifa() {
+        return 0.005;
+    }
+
+    @Override
+    public String getTipoConta() {
+        return "ContaPoupanca";
+    }
 }
