934a64344728324e59b9ad762abae89104e3090c
Murilo Schittino <146205147+Murilord18@users.noreply.github.com>
2026-05-11 12:48:47 -0300

    Add Test ContaPoupanca

--- src/test/java/br/com/banco/ContaPoupancaTest.java
+++ src/test/java/br/com/banco/ContaPoupancaTest.java
@@ -1,21 +1,49 @@
 package br.com.banco;
 
 import br.com.banco.templatemethod.ContaCorrente;
+import br.com.banco.templatemethod.ContaPoupanca;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ContaPoupancaTest {
 
     @Test
     void deveCalcularTarifaMensalCorretamente() {
-        ContaCorrente conta = new ContaCorrente("001-1", "João Silva", 1000.00);
-        assertEquals(20.00, conta.calcularTarifaMensal(), 0.001);
+        
+        ContaPoupanca conta = new ContaPoupanca("004-1", "Fernanda Rocha", 1000.00);
+        assertEquals(5.00, conta.calcularTarifaMensal(), 0.001);
     }
+
 
     @Test
     void deveRetornarSaldoSuficienteQuandoHaSaldo() {
-        ContaCorrente conta = new ContaCorrente("001-2", "Maria Souza", 500.00);
+        ContaPoupanca conta = new ContaPoupanca("004-2", "Roberto Alves", 200.00);
         assertEquals("Saldo suficiente", conta.verificarSaldoAposCobranca());
+    }
+
+    @Test
+    void tarifaDaPoupancaDeveSerMenorQueContaCorrente() {
+        ContaPoupanca poupanca = new ContaPoupanca("005-1", "Lucia Torres", 1000.00);
+        ContaCorrente corrente = new ContaCorrente("005-2", "Lucia Torres", 1000.00);
+
+        assertTrue(poupanca.calcularTarifaMensal() < corrente.calcularTarifaMensal());
+    }
+
+    @Test
+    void deveRetornarExtratoCorreto() {
+        ContaPoupanca conta = new ContaPoupanca("006-1", "Marcos Ferreira", 2000.00);
+        String extrato = conta.getExtrato();
+        assertEquals(
+                "ContaPoupanca{numeroConta='006-1', titular='Marcos Ferreira', saldo=2000.00, tarifaMensal=10.00, status='Saldo suficiente'}",
+                extrato
+        );
+    }
+
+    @Test
+    void deveRetornarTipoContaPoupanca() {
+        ContaPoupanca conta = new ContaPoupanca("007-1", "Beatriz Nunes", 500.00);
+        assertEquals("ContaPoupanca", conta.getTipoConta());
     }
 }
